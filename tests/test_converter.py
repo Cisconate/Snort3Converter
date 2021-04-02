b13ad3b65185aeba178ff96269e389044df047d8
Kyle Stapp <rabidcicada@gmail.com>
2021-04-02 14:24:46 -0400

    Massaging to be fully testable with tox

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -2,7 +2,6 @@ from snort3_conv_tools import snort3convert
 import os
 
 def test_function (tmp_path):
-    print(f"CWD: {os.getcwd()}")
     snort3convert.main(1000000, "SURICATA", "SNORT3", "../tests/data_files/testsuricatainputrules.txt", tmp_path/"testoutput.txt")
     with open("../tests/data_files/testsnort3outputrules.txt", 'r', encoding='utf-8') as file1:
         data1 = file1.readlines()
